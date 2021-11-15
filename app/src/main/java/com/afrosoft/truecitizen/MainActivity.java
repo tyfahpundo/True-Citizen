4e868939b7ac06e4dbc93b4484b001210c4fc320
tyfahpundo <tafadzwaallentyfah@gmail.com>
2021-11-15 07:39:34 +0200

    Implemented the true or false buttons

--- app/src/main/java/com/afrosoft/truecitizen/MainActivity.java
+++ app/src/main/java/com/afrosoft/truecitizen/MainActivity.java
@@ -4,17 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 
 import com.afrosoft.truecitizen.databinding.ActivityMainBinding;
 import com.afrosoft.truecitizen.model.Question;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     private ActivityMainBinding binding;
     private int currentQuestionIndex = 0;
 
-    private Question[] questionBank = new Question[] {
+    private final Question[] questionBank = new Question[] {
             //create/instantiate question objects
             new Question(R.string.question_amendments,false),
             new Question(R.string.question_constitution,true),
@@ -32,18 +32,36 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
+        binding.trueButton.setOnClickListener(v -> {
+            checkAnswer(true);
+        });
+        binding.falseButton.setOnClickListener(v -> {
+            checkAnswer(false);
+        });
+
         binding.questionTextView.setText(questionBank[currentQuestionIndex].getAnswerResId());
 
         binding.nextButton.setOnClickListener(v -> {
-            currentQuestionIndex = (currentQuestionIndex + 1)% questionBank.length;
+            currentQuestionIndex = (currentQuestionIndex + 1)% questionBank.length;//incrementing
             updateQuestion();
         });
         binding.prevButton.setOnClickListener(v -> {
             if(currentQuestionIndex > 0){
-                currentQuestionIndex = (currentQuestionIndex - 1)% questionBank.length;
+                currentQuestionIndex = (currentQuestionIndex - 1)% questionBank.length;//decrementing
                 updateQuestion();
             }
         });
+    }
+    private void checkAnswer(boolean userChoseCorrect){
+        boolean answerIsCorrect = questionBank[currentQuestionIndex].isAnswerTrue();
+        int messageId;
+        if(answerIsCorrect == userChoseCorrect){
+            messageId = R.string.correct_answer;
+        }else {
+            messageId = R.string.wrong_answer;
+        }
+        Snackbar.make(binding.imageView,messageId,Snackbar.LENGTH_SHORT)
+                .show();
     }
 
     private void updateQuestion() {
